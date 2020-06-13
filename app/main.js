6f2487c7848f72c4af8e7dbe4a711e199415cfe8
Franklyn Kleiton <franklynkleiton18@gmail.com>
2020-06-13 14:31:34 -0300

    :recycle: Refactoring To Vue Component

--- app/main.js
+++ app/main.js
@@ -1,4 +1,45 @@
-const app = new Vue({
+const submissionComponent = {
+  template: 
+  `
+  <div style="display: flex; width: 100%;">
+    <figure class="media-left">
+      <img class="image is-64x64" v-bind:src="submission.submissionImage" />
+    </figure>
+    <div class="media-content">
+      <div class="content">
+          <p>
+            <strong>
+              <a v-bind:href="submission.url" class="has-text-info">{{ submission.title }}</a>
+              <span class="tag is-small">#{{ submission.id }}</span>
+            </strong>
+            <br />
+            {{ submission.description }}
+            <br />
+            <small class="is-size-7">
+              Submitted by:
+              <image class="image is-24x24" v-bind:src="submission.avatar" />
+            </small>
+          </p>
+      </div>
+    </div>
+    <div class="media-right">
+      <span class="icon is-small">
+        <i class="fa fa-chevron-up" v-on:click="upvote(submission.id)"></i>
+        <strong class="has-text-info">{{ submission.votes }}</strong>
+       </span>
+    </div>
+  </div>
+  `,
+  props: ['submission', 'submissions'],
+  methods: {
+    upvote(id) {
+      const submission = this.submissions.find(el => el.id === id);
+      submission.votes++;
+    }
+  },
+};
+
+new Vue({
   el: '#app',
   data: {
     submissions: Seed.submissions
@@ -8,10 +49,7 @@ const app = new Vue({
   		return this.submissions.sort((a, b) => b.votes - a.votes);
   	}
   },
-  methods: {
-  	upvote(id) {
-  		const submission = this.submissions.find(el => el.id === id);
-  		submission.votes++;
-  	}
+  components: {
+    'submission-component': submissionComponent
   }
 });
